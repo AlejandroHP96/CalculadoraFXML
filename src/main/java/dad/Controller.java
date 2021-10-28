e67dbf7df0f21ab76b38549364322d6f3f9c428d
AlejandroHP96 <faillostherrera@gmail.com>
2021-10-28 18:46:15 +0100

    Proyecto Terminado

--- src/main/java/dad/Controller.java
+++ src/main/java/dad/Controller.java
@@ -10,7 +10,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
@@ -20,10 +19,6 @@ public class Controller implements Initializable {
 
     @FXML
     private GridPane view;
-
-    @FXML
-    private Button button7, button8, button9, buttonCE, buttonC, buttonIgual, button0, button4, button5, button6,
-            buttonMul, buttonDiv,button1,button2,button3,buttonRes,buttonComa,buttonSum;
 
     @FXML
     private TextField numTextField;
@@ -45,10 +40,83 @@ public class Controller implements Initializable {
 
     @FXML
     private void onClick(ActionEvent e) {
-        String button7, button8, button9, buttonCE, buttonC, buttonIgual, button0, button4, button5, button6,
-        buttonMul, buttonDiv,button1,button2,button3,buttonRes,buttonComa,buttonSum;
 
-        
+        char letra = e.getSource().toString().charAt(38);
+
+        switch (letra) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            calculadora.insertar(letra);
+            break;
+        case '+':
+            calculadora.operar(Calculadora.SUMAR);
+            break;
+        case '-':
+            calculadora.operar(Calculadora.RESTAR);
+            break;
+        case '*':
+            calculadora.operar(Calculadora.MULTIPLICAR);
+            break;
+        case '/':
+            calculadora.operar(Calculadora.DIVIDIR);
+            break;
+        case '.':
+            calculadora.insertarComa();
+            break;
+        case '=':
+            calculadora.operar(Calculadora.IGUAL);
+        default:
+            break;
+        }
+
+        // if (e.getSource().equals(button0)) {
+        // calculadora.insertar(button0.getText().charAt(0));
+        // } else if (e.getSource().equals(button1)) {
+        // calculadora.insertar(button1.getText().charAt(0));
+        // } else if (e.getSource().equals(button2)) {
+        // calculadora.insertar(button2.getText().charAt(0));
+        // } else if (e.getSource().equals(button3)) {
+        // calculadora.insertar(button3.getText().charAt(0));
+        // } else if (e.getSource().equals(button4)) {
+        // calculadora.insertar(button4.getText().charAt(0));
+        // } else if (e.getSource().equals(button5)) {
+        // calculadora.insertar(button5.getText().charAt(0));
+        // } else if (e.getSource().equals(button6)) {
+        // calculadora.insertar(button6.getText().charAt(0));
+        // } else if (e.getSource().equals(button7)) {
+        // calculadora.insertar(button7.getText().charAt(0));
+        // } else if (e.getSource().equals(button8)) {
+        // calculadora.insertar(button8.getText().charAt(0));
+        // } else if (e.getSource().equals(button9)) {
+        // calculadora.insertar(button9.getText().charAt(0));
+        // } else if (e.getSource().equals(buttonC)) {
+        // calculadora.borrar();
+        // } else if (e.getSource().equals(buttonCE)) {
+        // calculadora.borrarTodo();
+        // } else if (e.getSource().equals(buttonSum)) {
+        // calculadora.operar(Calculadora.SUMAR);
+        // }else if (e.getSource().equals(buttonRes)) {
+        // calculadora.operar(Calculadora.RESTAR);
+        // } else if (e.getSource().equals(buttonDiv)) {
+        // calculadora.operar(Calculadora.DIVIDIR);
+        // } else if (e.getSource().equals(buttonMul)) {
+        // calculadora.operar(Calculadora.MULTIPLICAR);
+        // } else if (e.getSource().equals(buttonIgual)) {
+        // calculadora.operar(Calculadora.IGUAL);
+        // } else {
+        // calculadora.insertarComa();
+        // }
+
+        textProperty.set(calculadora.getPantalla());
+
     }
 
     public GridPane getview() {
